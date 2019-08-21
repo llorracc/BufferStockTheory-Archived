40ae5245bd0dcd3d543cf9c3e8c5e94a4844bdb6
llorracc <llorracc@users.noreply.github.com>
2019-08-21 10:09:15 -0400

    Add cPDV = MPCnvrs; minor edits

--- texmf-local/tex/latex/tex4htMakeCFG.sh
+++ texmf-local/tex/latex/tex4htMakeCFG.sh
@@ -6,32 +6,10 @@ then
   exit 1
 fi
 
-echo "\\Preamble{html}"       > $1.cfg
-printf '\\begin{document}' >> $1.cfg
-echo "\HCode{         " >> $1.cfg
-printf "<meta name = \042Author\042      content = \042Christopher D. Carroll\042> \Hnewline \n" >>$1.cfg
-printf "<meta name = \042Description\042 content = \042" >>$1.cfg
-(cat $1.title | tr -d '\012') >> $1.cfg
-printf "\042> \Hnewline \n" >>$1.cfg
-printf "<title>"  >> $1.cfg 
-(cat $1.title | tr -d '\012') >> $1.cfg
-printf "</title> \Hnewline" >> $1.cfg
-echo "}" >> $1.cfg
-read -r -d '' CenterTabsAndFigs << 'EOF'
-\ConfigureEnv{figure}
-   {\HCode{<hr class="figure"><center class="figure"\Hnewline}
-       \bgroup \Configure{float}{\ShowPar}{}{}%
-   }
-   {\egroup \HCode{</center><hr class="endfigure"}\ShowPar
-\par}
-   {}{}
-\ConfigureEnv{table}
-   {\EndP \HCode{<center class="table">}}
-   {\EndP \HCode{</center>}\par\ShowPar}
-   {}{}
-EOF
-echo "$CenterTabsAndFigs" >> $1.cfg
-printf '\\EndPreamble' >> $1.cfg
-echo '' >> $1.cfg
+handoutName=$1
 
+# cd "$(dirname "$0")" # http://stackoverflow.com/questions/3349105/how-to-set-current
+
+cp `kpsewhich svg-math-and-subfigures.cfg` $handoutName.cfg
+cp `kpsewhich svg-set-size-to-1p0.mk4` $handoutName.mk4
 
