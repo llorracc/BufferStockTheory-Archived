c3e8fad1100ef98782fa39fa66a34926aece5f63
llorracc <git@llorracc.org>
2020-10-04 14:36:55 -0400

    makePublic autoupate

--- reproduce.sh
+++ reproduce.sh
@@ -1,3 +1,2 @@
-#!/bin/bash
-sudo docker run -v "$PWD":/home/jovyan/work -it --rm econark/econ-ark-notebook:master bash -c "export TERM=dumb; cd Code/Python ipython do_all.py"
-
+cd Code/Python
+ipython BufferStockTheory-Problems-and-Solutions-Source.py
