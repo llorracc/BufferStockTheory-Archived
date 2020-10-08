427a1777510831ae0290ef967c0cbb9a25ceed92
llorracc <git@llorracc.org>
2020-10-08 17:46:44 -0400

    makePublic autoupate

--- reproduce.sh
+++ reproduce.sh
@@ -1,14 +1,16 @@
 cd Code/Python
 ipython BufferStockTheory-Problems-and-Solutions-Source.ipynb
 
-# Compile Latex files
+# Compile LaTeX files
 cd ../../
 for file in BufferStockTheory-NoAppendix BufferStockTheory BufferStockTheory-Slides; do
-	pdflatex "$file.tex"
-	bibtex "$file"
-	pdflatex "$file.tex"
-	pdflatex "$file.tex"
+    pdflatex -output-directory=LaTeX "$file.tex"
+    pdflatex -output-directory=LaTeX "$file.tex"
+    bibtex LaTeX/"$file"
+    pdflatex -output-directory=LaTeX "$file.tex"
 done
+
+# Remove LaTeX junk files
 
 for file in BufferStockTheory-NoAppendix BufferStockTheory BufferStockTheory-Slides; do
     rm "$file.aux" "$file.bbl" "$file.blg" "$file.dep" "$file.log" "$file.out"
